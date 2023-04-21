bf7aa5de4a0ee273a454ef46b83a4ebe5988c570
Pain <s.nurken92@gmail.com>
2023-04-21 23:58:34 +0600

    fix swagger

--- app/core/urls.py
+++ app/core/urls.py
@@ -7,7 +7,7 @@ from core.swagger import swagger_patterns
 
 urlpatterns = [
     path('__debug__/', include('debug_toolbar.urls')),
-    path('sapi/v1/wagger/', include(swagger_patterns)),
+    path('api/v1/swagger/', include(swagger_patterns)),
 
     path('admin/', admin.site.urls),
 
