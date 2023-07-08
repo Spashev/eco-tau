e8d5b5abe0da3de10e8e930c8f1d75c22a319608
Nurken Spashev <nurken.spashev@bloomex.ca>
2023-07-08 23:00:08 +0400

    rm old migration

--- app/account/views.py
+++ app/account/views.py
@@ -26,7 +26,7 @@ class UserViewSet(
     queryset = User.objects.all()
     serializer_class = ListUserSerializer
     filterset_fields = ['email']
-    permission_classes = (permissions.IsAdminUser, permissions.IsAuthenticatedOrReadOnly)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     def get_serializer_class(self):
         serializer = self.serializer_class
