ba7887e227ec652b6d516664ab5d1f654f94833d
Spashev Nurken <s.nurken92@gmail.com>
2023-08-13 20:54:07 +0400

    jwt

--- app/core/settings/development.py
+++ app/core/settings/development.py
@@ -3,6 +3,8 @@ from datetime import timedelta
 # django-rest-framework
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
         "account.authentication.JWTAuthentication",
     ),
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
